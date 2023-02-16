aee2a3785fb178875b270532349365006c036f21
gagan goyal <goyalgagan82@gmail.com>
2023-02-16 22:52:49 +0530

    add student show page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,16 +4,33 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
   def new
+    @student = Student.new
   
   end
 
   def create
-  
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:success] = "You have successfully signed up"
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def update
   
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :email)
   end
 
 end
