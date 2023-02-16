3dfc752cbe6f5a0098a3a3df97816c146ef1e691
gagan goyal <goyalgagan82@gmail.com>
2023-02-16 19:29:16 +0530

    add student controller

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,19 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def new
+  
+  end
+
+  def create
+  
+  end
+
+  def update
+  
+  end
+
+end
